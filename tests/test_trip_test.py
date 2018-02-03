de4e54054bb54ab90a9fbf521873bc124b31cefe
Solomon Bothwell <ssbothwell@gmail.com>
2018-02-03 01:06:28 -0800

    REFACTOR: adds helper function for row insertion

--- tests/test_trip_test.py
+++ tests/test_trip_test.py
@@ -12,11 +12,20 @@ class TripTestCase(unittest.TestCase):
         self.app = trip_test.app.test_client()
         with trip_test.app.app_context():
             trip_test.trip_test.init_db()
+        
+        # Load an initial mock member
+        name = 'initial user'
+        email = 'initial@user.foo'
+        phone = '9999999999'
+        mock_data = dict(name=name, email=email, phone=phone)
+
+        self.generate_mock_data('admin_user', mock_data)
 
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(trip_test.app.config['DATABASE'])
+
 
     def login(self, username, password):
         headers = {'content-type': 'application/json'}
@@ -26,44 +35,44 @@ class TripTestCase(unittest.TestCase):
         json_response = json.loads(response.get_data(as_text=True))
         return  json_response['access_token']
 
+
+    def generate_mock_data(self, user: str, data: dict):
+        """ Insert a row into members table using a put request """
+        # Login
+        access_token = self.login(user, 'password')
+
+
+        headers = {'content-type': 'application/json',
+                   'Authorization': 'Bearer %s' % access_token}
+        response = self.app.put('/',
+                                data=json.dumps(data),
+                                headers=headers)
+        return response
+
+
     def test_putA(self):
         """ test put request success with admin user account """
-
-        # Login
-        access_token = self.login('admin_user', 'password')
 
         # Mock member data
         name = 'foobar'
         email = 'foo@bar.baz'
         phone = '8001234567'
+        mock_data = dict(name=name, email=email, phone=phone)
 
-        headers = {'content-type': 'application/json',
-                   'Authorization': 'Bearer %s' % access_token}
-        response = self.app.put('/',
-                                data=json.dumps(dict(name=name,
-                                                     email=email,
-                                                     phone=phone)),
-                                headers=headers)
+        response = self.generate_mock_data('admin_user', mock_data)
         self.assertEqual(response.status_code, 201)
+
 
     def test_putB(self):
         """ test put request failure with unprivileged user account """
 
-        # Login
-        access_token = self.login('get_user', 'password')
-
         # Mock member data
         name = 'foobar'
         email = 'foo@bar.baz'
         phone = '8001234567'
+        mock_data = dict(name=name, email=email, phone=phone)
 
-        headers = {'content-type': 'application/json',
-                   'Authorization': 'Bearer %s' % access_token}
-        response = self.app.put('/',
-                                data=json.dumps(dict(name=name,
-                                                     email=email,
-                                                     phone=phone)),
-                                headers=headers)
+        response = self.generate_mock_data('get_user', mock_data)
         with self.subTest():
             self.assertEqual(response.status_code, 200)
         with self.subTest():
