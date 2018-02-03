790a115710e2ff68fbc00625b631084fe1ea8385
Solomon Bothwell <ssbothwell@gmail.com>
2018-02-02 18:15:55 -0800

    FEATURE: Implements basic JWT auth

--- schema.sql
+++ schema.sql
@@ -1,7 +1,22 @@
 drop table if exists members;
+drop table if exists users;
 create table members (
-    memberID integer primary key autoincrement,
-    name text not null,
-    email text not null,
-    phone text not null
+  memberID integer primary key autoincrement,
+  name text not null,
+  email text not null,
+  phone text not null
 );
+create table users (
+  userID integer primary key autoincrement,
+  username text not null,
+  password text not null,
+  access_rights integer default 0
+);
+INSERT INTO users (username, password, access_rights)
+       VALUES ('nothing_user', 'password', 0);
+INSERT INTO users (username, password, access_rights)
+       VALUES ('get_user', 'password', 1);
+INSERT INTO users (username, password, access_rights)
+       VALUES ('getput_user', 'password', 2);
+INSERT INTO users (username, password, access_rights)
+       VALUES ('admin_user', 'password', 3);
--- trip_test.py
+++ trip_test.py
@@ -2,7 +2,11 @@ import os
 import sqlite3
 import re
 from flask import (Flask, request, session, g, jsonify,
-                   abort, render_template)
+                   abort, url_for, redirect, render_template)
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 
 # Create application instance and load config
@@ -17,6 +21,11 @@ app.config.update(dict(
     PASSWORD='default'
 ))
 app.config.from_envvar('TRIP_SETTINGS', silent=True)
+
+# Setup the Flask-JWT-Extended extension
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
 
 
 def connect_db():
@@ -57,44 +66,131 @@ def initdb_command():
     print('Database initialized')
 
 
-@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
     db = get_db()
-    query = db.execute('SELECT name, email, phone FROM members order by memberID desc')
+    query = db.execute('SELECT memberID, name, email, phone FROM members order by memberID desc')
     entries = query.fetchall() 
     return render_template('index.html', entries=entries)
 
 
-@app.route('/', methods=['PUT'])
-def add_entry():
+@app.route('/', methods=['GET'])
+@jwt_required
+def get_entry():
+    # Validate access rights
+    username, access_rights = get_jwt_identity()
+    if access_rights == 0:
+        return jsonify({"msg": "Access Denied"}, 200)
+
+    # Validate request
     if not valid_json(request):
-        return abort(400)
+        return jsonify({"msg": "Missing JSON in request"}, 400)
+
+    # Query database
+    db = get_db()
+    memberID = request.json['memberID']
+
+    query = db.execute('SELECT * FROM members where memberID=?', [memberID]).fetchall()
+    entries = [ { 'memberID': row['memberID'],
+                  'name': row['name'],
+                  'phone': row['phone'],
+                  'email': row['email']
+                }
+                for row in query]
+    return jsonify(entries, 200)
+
+
+@app.route('/', methods=['PUT'])
+@jwt_required
+def add_entry():
+    # Validate access rights
+    username, access_rights = get_jwt_identity()
+    if access_rights <= 1:
+        return jsonify({"msg": "Access Denied"}, 200)
+    
+    # Validate request
+    if not valid_json(request):
+        return jsonify({"msg": "Missing JSON in request"}), 400
     if not (valid_email(request.json['email']) or 
             valid_phone(request.json['phone'])):
-        return abort(400)
+        return jsonify({"msg": "Please enter valid email and phone numbers"}), 400
 
-    member = { 'name': request.json['name'],
-               'email': request.json['email'],
-               'phone': request.json['phone']
-             }
-
+    name = request.json['name']
+    email = request.json['email']
+    phone = request.json['phone']
+          
+    # Query Database
     db = get_db()
-
-    # Check if the user already has an entry
     existing_user = db.execute('SELECT name FROM members WHERE name=?',
-                               [member['name']]).fetchall()
-
+                               [name]).fetchall()
+    # Update row
     if existing_user:
         db.execute('UPDATE members SET phone=?,email=? WHERE name=?', 
-                   [member['phone'], member['email'], member['name']])
+                   [phone, email, name])
+    # Create row
     else:
         db.execute('INSERT INTO members (name, email, phone) VALUES (?, ?, ?)',
-                     [member['name'], member['email'], member['phone']])
+                     [name, email, phone])
     db.commit()
-    return jsonify({'member': member}, 201)
+    return jsonify({"msg": "success"}), 201
 
 
-## Helper functions
+@app.route('/', methods=['DELETE'])
+@jwt_required
+def delete_entry():
+    # Validate access rights
+    username, access_rights = get_jwt_identity()
+    if access_rights <= 2:
+        return jsonify({"msg": "Access Denied"}, 200)
+
+    # Validate request
+    if not valid_json(request):
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    db = get_db()
+    memberID = request.json['memberID']
+
+    # Check for entry
+    existing_user = db.execute('SELECT name FROM members WHERE memberID=?',
+                               [memberID]).fetchall()
+    if existing_user:
+        db.execute('DELETE FROM members WHERE memberID=?', [memberID])
+        db.commit()
+        return jsonify({"msg": "success"}), 200
+    return jsonify({"msg": "no such entry"}), 404
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    """ Accept login info and return JWT token """
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    # Get user access rights from database
+    db = get_db()
+    query = db.execute('SELECT * FROM users where username=?', [username]).fetchall()
+    if query:
+        db_password = query[0]['password']
+        # Wrong password
+        if password != db_password:
+            return jsonify({"msg": "Bad username or password"}), 401
+        # Success
+        else:
+            access_token = create_access_token(identity=[username, query[0]['access_rights']])
+            return jsonify(access_token=access_token), 200
+    # No such username
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+
+
+### Validation Helper functions
 
 def valid_phone(phone_number: str) -> bool:
     pattern = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$', re.VERBOSE)
@@ -110,8 +206,15 @@ def valid_json(request) -> bool:
     """ ensure json requests contain correct fields """
     if not request.json:
         return False
-    if (not 'name' in request.json or 
-        not 'email' in request.json or 
-        not 'phone' in request.json):
+
+    if ((request.method == 'DELETE' or
+        request.method == 'GET') and 
+        not 'memberID' in request.json):
+        return False
+
+    if (request.method == 'PUT' and 
+        (not 'name' in request.json or 
+         not 'email' in request.json or 
+         not 'phone' in request.json)):
         return False
     return True
