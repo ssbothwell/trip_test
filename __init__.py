0017467fec200b6faa71b0ff41f14ea0265d8d79
Solomon Bothwell <ssbothwell@gmail.com>
2018-02-03 17:56:39 -0800

    REFACTOR: Added some comments to __init__.py

--- __init__.py
+++ __init__.py
@@ -9,11 +9,16 @@ import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
 
+### Configuration
+
 # Create application instance and load config
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 # Default config
+# secret keys and database user info MUST be overwritten
+# on production server using environment variables
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'trip_test.db'),
     SECRET_KEY='development key',
